4d8983634578c4584adaa8532d4473c5fc23f8f9
Ronen Hilewicz <ronen@aserto.com>
2022-02-23 12:20:04 -0500

    Remove unused constants

--- pkg/x/constants.go
+++ pkg/x/constants.go
@@ -1,18 +1,10 @@
 package x
 
 const (
-	Aserto         string = "aserto"
-	AppDisplayName string = "Aserto CLI"
 	AppDescription string = "Welcome to modern authorization!"
 
 	// AppName -- application name
 	AppName = "aserto"
 	// AppVersionTag -- version tag
 	AppVersionTag = ""
-	// DefaultEnv -- default service environment
-	DefaultEnv string = EnvProduction
-
-	// environments.
-	EnvProduction string = "prod"
-	Target               = AppName
 )
