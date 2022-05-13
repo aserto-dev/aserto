354458f1191b5e78579a302a2885dab072a99502
Stefan <stefan@aserto.com>
2022-05-13 22:02:18 +0000

    fix typo

--- pkg/cmd/control_plane.go
+++ pkg/cmd/control_plane.go
@@ -9,7 +9,7 @@ type ControlPlaneCmd struct {
 	ListConnections           controlplane.ListConnectionsCmd           `cmd:"" help:"list satellite connections" group:"control-plane"`
 	ClientCert                controlplane.ClientCertCmd                `cmd:"" help:"get client certificates for a satellite connection" group:"control-plane"`
 	ListInstanceRegistrations controlplane.ListInstanceRegistrationsCmd `cmd:"" help:"list instance registrations" group:"control-plane"`
-	Discovery                 controlplane.DiscoveryCmd                 `cmd:"" help:"run discovery on an registered instance" group:"control-plane"`
+	Discovery                 controlplane.DiscoveryCmd                 `cmd:"" help:"run discovery on a registered instance" group:"control-plane"`
 }
 
 func (cmd *ControlPlaneCmd) Run(c *cc.CommonCtx) error {
