fe8f8808bbbeb10703ab0bd5a0084c0f83d10dd4
oanatmaria <oana@aserto.com>
2023-09-06 16:38:29 +0300

    Rebase & fix lint

--- pkg/handlers/directory/graph.go
+++
@@ -1,9 +0,0 @@
-package directory
-
-import "github.com/aserto-dev/aserto/pkg/cc"
-
-type GetGraphCmd struct{}
-
-func (cmd *GetGraphCmd) Run(c *cc.CommonCtx) error {
-	return nil
-}
--- pkg/handlers/directory/load.go
+++
@@ -1,9 +0,0 @@
-package directory
-
-import "github.com/aserto-dev/aserto/pkg/cc"
-
-type LoadCmd struct{}
-
-func (cmd *LoadCmd) Run(c cc.CommonCtx) error {
-	return nil
-}
