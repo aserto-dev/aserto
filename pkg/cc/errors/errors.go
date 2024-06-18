bfa80e146d1f4760d5d4133d1ea120b337646db5
Gert Drapers <gert.drapers@live.com>
2024-06-18 11:05:00 -0700

    rm mage

--- pkg/cc/errors/errors.go
+++ pkg/cc/errors/errors.go
@@ -6,7 +6,8 @@ import (
 )
 
 var (
-	NeedLoginErr    = errors.Errorf("user is not logged in, please login using '%s login'", x.AppName)
-	TokenExpiredErr = errors.Errorf("the access token has expired, please login using '%s login'", x.AppName)
-	NeedTenantIDErr = errors.Errorf("operation requires tenant-id, please login using '%s login' or use --tenant to specify an id.", x.AppName)
+	NeedLoginErr     = errors.Errorf("user is not logged in, please login using '%s login'", x.AppName)
+	TokenExpiredErr  = errors.Errorf("the access token has expired, please login using '%s login'", x.AppName)
+	NeedTenantIDErr  = errors.Errorf("operation requires tenant-id, please login using '%s login' or use --tenant to specify an id.", x.AppName)
+	ResolveTenantErr = errors.New("cannot resolve tenant name %q to tenant ID")
 )
