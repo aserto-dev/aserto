142f55f85d66294304a0677708ed5b38b003d50e
Gert Drapers <gert.drapers@live.com>
2024-06-27 23:57:37 -0700

    rework control plane commands

--- pkg/cmd/control_plane.go
+++ pkg/cmd/control_plane.go
@@ -4,15 +4,13 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/aserto-dev/aserto/pkg/cc"
 	"github.com/aserto-dev/aserto/pkg/cc/errors"
-	controlplane "github.com/aserto-dev/aserto/pkg/handlers/control_plane"
+	cp "github.com/aserto-dev/aserto/pkg/handlers/control_plane"
 )
 
 type ControlPlaneCmd struct {
-	ListConnections           controlplane.ListConnectionsCmd           `cmd:"" help:"list edge authorizer connections" group:"control-plane"`
-	ClientCert                controlplane.ClientCertCmd                `cmd:"" help:"get client certificates for an edge authorizer connection" group:"control-plane"`
-	ListInstanceRegistrations controlplane.ListInstanceRegistrationsCmd `cmd:"" help:"list instance registrations" group:"control-plane"`
-	Discovery                 controlplane.DiscoveryCmd                 `cmd:"" help:"run discovery on a registered instance" group:"control-plane"`
-	EdgeDirSync               controlplane.EdgeDirSyncCmd               `cmd:"" help:"sync the directory on an edge authorizer" group:"control-plane"`
+	Get  ControlPlaneGetCmd  `cmd:"" help:"certificates"`
+	List ControlPlaneListCmd `cmd:"" help:"list connections | instances"`
+	Exec ControlPlaneExecCmd `cmd:"" help:"exec discovery | edge-sync"`
 }
 
 func (cmd *ControlPlaneCmd) BeforeApply(context *kong.Context) error {
@@ -29,4 +27,18 @@ func (cmd *ControlPlaneCmd) BeforeApply(context *kong.Context) error {
 
 func (cmd *ControlPlaneCmd) Run(c *cc.CommonCtx) error {
 	return nil
+}
+
+type ControlPlaneGetCmd struct {
+	Certificates cp.ClientCertCmd `cmd:"" help:"get client certificates for an edge authorizer connection"`
+}
+
+type ControlPlaneListCmd struct {
+	Connections cp.ListConnectionsCmd           `cmd:"" help:"list edge authorizer connections"`
+	Instances   cp.ListInstanceRegistrationsCmd `cmd:"" help:"list instance registrations"`
+}
+
+type ControlPlaneExecCmd struct {
+	Discovery cp.DiscoveryCmd   `cmd:"" help:"run discovery on a registered instance"`
+	EdgeSync  cp.EdgeDirSyncCmd `cmd:"" help:"sync the directory on an edge authorizer"`
 }
